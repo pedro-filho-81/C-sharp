8347f2e7965861127b61226fa395bffad2070b94
PedroFilho <psfilho81@hotmail.com>
2021-03-06 16:38:41 -0300

    Adicionar arquivos de projeto.

---
+++ CadeiaDeCaracteres/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CadeiaDeCaracteres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variáveis
+            string fristFriend = "Pedro";
+            string secoundFriend = "Cleonice";
+            
+            // imprima
+            Console.WriteLine($"My friends are {fristFriend} and {secoundFriend}");
+            
+            // a propriedade length mostra o tamanho da string
+            Console.WriteLine($"The name {fristFriend} has {fristFriend.Length} lettres.");
+            Console.WriteLine($"The name {secoundFriend} has {secoundFriend.Length} lettres.");
+
+            Console.ReadLine();
+        }
+    }
+}
