3ff0ba598d309ece89d051f9db44b37267cdc6f6
PedroFilho <psfilho81@hotmail.com>
2021-03-11 20:03:41 -0300

    Create Program.cs

---
+++ PrimeiroPrograma/PrimeiroPrograma/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PrimeiroPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // imprime
+            Console.WriteLine("Olá, Mundo!");
+            Console.ReadLine(); // aguarda teclalr entra
+        }
+    }
+}
