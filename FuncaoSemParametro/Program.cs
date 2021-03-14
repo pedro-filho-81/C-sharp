ab0593e42e074a7ecf53bcbab579cabff82afc91
PedroFilho <psfilho81@hotmail.com>
2021-03-14 16:21:45 -0300

    Create Program.cs

---
+++ FuncaoSemParametro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FuncaoSemParametro
+{
+    class Program
+    {
+        // cria uma função sem parametro
+        public void Mostrar() // sem parâmetro
+        {
+            // imprime
+            Console.WriteLine("Esta é uma função não parametrizada.");
+        }
+        static void Main(string[] args)
+        {
+            // cria o objeto da classe programa
+            Program programa = new Program();
+
+            // chama a função criada acima Mostrar
+            programa.Mostrar();
+
+        } // fim de main
+    } // fim classe
+} // fim namespace
