984323c3644cfa5cf8d9478e4859a58253997736
PedroFilho <psfilho81@hotmail.com>
2021-03-25 15:59:38 -0300

    Atualização

--- CSharp_JavaComoProgramar/Cap002_JavaComoProgramar_C_Sharp/ComparandoInteiros2_16/Program.cs
+++ CSharp_JavaComoProgramar/Cap002_JavaComoProgramar_C_Sharp/ComparandoInteiros2_16/Program.cs
@@ -9,14 +9,26 @@ namespace ComparandoInteiros2_16
             // variáveis
             int num1, num2;
 
+            // cabeçalho
+            Console.ForegroundColor = ConsoleColor.White; // letras brancas
+            Console.BackgroundColor = ConsoleColor.DarkBlue; // fundo azul
+            Console.WriteLine("COMPARANDO NÚMEROS INTEIROS");
+            Console.ResetColor(); 
+
             // entrada de dados
             Console.Write("Primeiro número: ");
             int.TryParse(Console.ReadLine(), out num1);
             Console.Write("Segundo número: ");
             int.TryParse(Console.ReadLine(), out num2);
 
+            // cabeçalho resultado
+            Console.ForegroundColor = ConsoleColor.Green; // letras brancas
+            Console.BackgroundColor = ConsoleColor.Yellow; // fundo azul
+            Console.WriteLine("COMPARANDO NÚMEROS INTEIROS");
+            Console.ResetColor();
+
             // condições de verificação
-            if( num1 > num2)
+            if ( num1 > num2)
             {
                 Console.WriteLine($"[{num1, -5}] é maior que [{num2, 5}]");
             }
@@ -28,8 +40,9 @@ namespace ComparandoInteiros2_16
             {
                 Console.WriteLine($"{num1} é igual a {num2}");
             }
+            
+            // cabeçalho
             Console.ReadKey();
-
             Console.WriteLine("Hello World!");
         }
     }
