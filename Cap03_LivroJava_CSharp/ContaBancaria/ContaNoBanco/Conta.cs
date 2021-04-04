4090c6908df6c60b85bd3c07751556d65548274c
PedroFilho <psfilho81@hotmail.com>
2021-04-04 10:09:08 -0300

    Conta Bancária

---
+++ Cap03_LivroJava_CSharp/ContaBancaria/ContaNoBanco/Conta.cs
@@ -0,0 +1,53 @@
+using System; // biblioteca obrigatória
+
+class Conta // cria a classe Conta
+{
+    // cria propriedade do tipo string Nome
+    public string Nome { get; set; }
+    
+    // cria variável privada do tipo decimal de nome saldo 
+    private decimal saldo;
+
+    // cria construtor que tem de ter o mesmo nome da classe
+    public Conta( string nome, decimal saldoInicial )
+    {
+        // atribui a variável nome a propriedade Nome
+        this.Nome = nome;
+        // atribui a variável saldoInicial a propriedade Saldo
+        this.Saldo = saldoInicial;
+    } // fim construtos
+
+    // cria a propriedade Saldo
+    public decimal Saldo
+    {
+        // que
+        get
+        {
+            // retorna o valor a variável saldo
+            return saldo;
+        } // fim get
+        // cria set
+        private set
+        {
+            // verifica se o valor informado no construtor é maior que zero
+            if( value > 0.0M ) // se verdade
+            {
+                // a variável saldo recebe o valor
+                saldo = value;
+            } // fim if
+            
+        } // fim set
+    } // fim propriedade Saldo
+
+    // cria o método Depósito
+    public void Deposito( decimal depositoNaConta )
+    {
+        // se o depóeiso for maior que zero
+        if( depositoNaConta > 0.0M)
+        {
+            // saldo soma mais depósito na conta
+            Saldo += depositoNaConta;
+        } // fim if
+    } // fim método Deposito
+
+} // fim classe
---
+++ Cap03_LivroJava_CSharp/ContaBancaria/ContaNoBanco/Program.cs
@@ -0,0 +1,63 @@
+﻿using System; // biblioteca básica 
+
+namespace ContasBancaria // local para organizar as classes
+{
+    class Program // nome da classe
+    {
+        // método principal da classe Program
+        static void Main(string[] args)
+        {
+            // cria o objeto minha conta da classe Conta
+            // palavra chave NEW cria o objeto e atribui valor
+            // minhaConta é o objeto criado
+            Conta minhaConta1 = new Conta("Pedro", 50M);
+            Conta minhaConta2 = new Conta("Cleonice", -7.53M);
+
+            // mostra resultado inicial
+            Console.WriteLine($"O nome da conta é {minhaConta1.Nome} " + 
+                $"meu saldo inicial é {minhaConta1.Saldo:C}");
+            Console.WriteLine($"O nome da conta é {minhaConta2.Nome} " + 
+                $"meu saldo é {minhaConta2.Saldo:C}");
+
+            // entrada de dados
+            System.Console.WriteLine("Entre com o nome da conta1: ");
+            // cria a variável do tipo string que recebe do usuário o nome da conta1
+            string nomeDaConta1 = Console.ReadLine();
+            // chama o objeto e atribui o nome da conta1
+            minhaConta1.Nome = nomeDaConta1;
+
+            // entrada de dados do depósito da conta 1
+            System.Console.WriteLine("Entre com o valor de depóeito da conta 1: " );
+            // cria a variável que recebe do usuário da conta 1
+            decimal depositoDaConta1 = decimal.Parse(Console.ReadLine());
+            // chama o método Depósito da classe Conta e atribui o valor do depósito
+            minhaConta1.Deposito( depositoDaConta1 );
+
+            // entrada de dados
+            System.Console.WriteLine("Entre com o nome da conta2: ");
+            // cria a variável que recebe do usuário o nome da conta1
+            string nomeDaConta2 = Console.ReadLine();
+            // chama o objeto e atribui o nome da conta1
+            minhaConta2.Nome = nomeDaConta2;
+
+            // entrada de dados do depósito da conta 1
+            System.Console.WriteLine("Entre com o valor de depóeito da conta2: " );
+            // cria a variável que recebe do usuário da conta 1
+            decimal depositoDaConta2 = decimal.Parse(Console.ReadLine());
+            // chama o método Depósito da classe Conta e atribui o valor do depósito
+            minhaConta2.Deposito( depositoDaConta2 );
+            
+            // mostra o nome e o valor da conta1
+            System.Console.WriteLine($"O nome e da conta1 é {minhaConta1.Nome} " +
+                $"com o saldo de {minhaConta1.Saldo:C}");
+
+            // mostra o nome e o valor da conta1
+            System.Console.WriteLine($"O nome e da conta2 é {minhaConta2.Nome} " +
+                $"com o saldo de {minhaConta2.Saldo:C}");
+
+            Console.ReadLine(); // pausa programa
+            Console.Clear(); // limpa a tela
+
+        } // fim main
+    } // fm classe
+} // fim namespace
