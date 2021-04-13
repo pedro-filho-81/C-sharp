069b6abc408e17228b1000585d88f6eefd38dfdc
PedroFilho <psfilho81@hotmail.com>
2021-04-13 10:54:23 -0300

    Update Program.cs

--- ExerciciosCSharp/CalculadorDeSalarios/Program.cs
+++ ExerciciosCSharp/CalculadorDeSalarios/Program.cs
@@ -5,6 +5,18 @@ namespace CalculadorDeSalarios
 {
     class Program
     {
+        /*
+            4.20 (Calculador de salários) Desenvolva um aplicativo Java que determina 
+            o salário bruto de cada um de três empregados. A empresa paga  as horas normais 
+            pelas primeiras 40 horas trabalhadas por cada funcionário e 50% a mais por 
+            todas as horas trabalhadas além das 40  horas. Você recebe uma lista de empregados, 
+            o número de horas trabalhadas por eles na semana passada e o salário-hora de 
+            cada um. Seu  programa deve aceitar a entrada dessas informações para cada empregado 
+            e, então, determinar e exibir o salário bruto do empregado.  Utilize a classe Scanner 
+            para inserir os dados. 
+            Deitel, Paul; Deitel, Harvey. Java: como programar (p. 115). Edição do Kindle. 
+            Autor: Pedro Filho - 13/04/2021
+        */
         static void Main(string[] args)
         {    
             // variáveis
