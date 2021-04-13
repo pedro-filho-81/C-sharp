806e1bf2198c13a8e10b2fec0bdae59035a65d3f
PedroFilho <psfilho81@hotmail.com>
2021-04-13 10:47:58 -0300

    Calculador de Salário

---
+++ CSharpComoProgramar/LivroCSharpComoProgramar/BemVindo/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace BemVindo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.BackColor = System.Drawing.Color.White;
+            this.lblMsg.Font = new System.Drawing.Font("Arial", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMsg.Location = new System.Drawing.Point(242, 136);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(71, 40);
+            this.lblMsg.TabIndex = 0;
+            this.lblMsg.Text = "Olá";
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(269, 179);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(327, 174);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "button1";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.lblMsg);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Button btn;
+    }
+}
+
---
+++ ExerciciosCSharp/CalculadorDeSalarios/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using static System.Console;
+
+namespace CalculadorDeSalarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            // variáveis
+            string func;
+            decimal valorDaHora = 0m;
+            decimal salario = 0m;
+            int horasTrabalhadas = 0;
+            int sentinela = 1;
+
+            // enquanto contador menor ou igual a 3
+            while( sentinela != -1 )
+            {
+                Clear(); // limpa a tela
+
+                // entrada de dados
+                Write( "Nome do funcionário: " );
+                // aguarda entrada do usuário
+                func = ReadLine();
+
+                Write("Valor das horas trabalhadas R$" );
+                // aguarda usuário
+                valorDaHora = decimal.Parse( ReadLine() );
+                
+                Write("Total de horas trabalhadas:");
+                // aguarda usuário
+                horasTrabalhadas = int.Parse(ReadLine());
+
+                // cálculo do salário
+                // se horas trabalhadas menor ou igual a 40 horas
+                if( horasTrabalhadas <= 40 )
+                {
+                    // salario recebe
+                    salario = valorDaHora * horasTrabalhadas;
+                } // fim if
+                else // se não
+                {
+                    // cria variável horas extras e atribui o valor das extras
+                    decimal horasExtras = ( horasTrabalhadas - 40 ) * (valorDaHora / 2 );
+                    // salario recebe o valor da expressão
+                    salario = ( valorDaHora * 40 ) + horasExtras;
+                } // fim else
+
+                // mostra resultado
+                WriteLine($"\nFuncionário {func}\nTrabalhou {horasTrabalhadas} horas");
+                WriteLine($"Salário {salario:C}" );
+                
+                WriteLine(); // pula linha
+
+                Console.Write("Deseja cadastrar outro funcionário? [ 1=sim / -1=não ]: " );
+                sentinela = int.Parse(ReadLine());
+
+                Clear(); // limpa a tela
+
+            } // fim while
+        } // fim main
+    } // fim classe
+} // fim namespace
