8b1104c4bb98c81a069523e8177f12c8f26fd21d
PedroFilho <psfilho81@hotmail.com>
2021-04-14 10:11:09 -0300

    Update Program.cs

--- ExerciciosCSharp/Misterio2/Program.cs
+++ ExerciciosCSharp/Misterio2/Program.cs
@@ -6,7 +6,21 @@ namespace Misterio2
     {
         static void Main(string[] args)
         {
+            // cria variável
+            int contador = 1;
+            // conte até 10
+            while( contador <= 10 )
+            {
+                // imprima asterisco se o valor for ímpar
+                Console.WriteLine(contador % 2 == 1 ? "*****" : "++++++++++" );
+
+                contador++; // soma 1 ao contador
+            } // fim while
+
             Console.WriteLine("Hello World!");
-        }
-    }
-}
+            Console.ReadKey();
+            Console.Clear();
+
+        } // fim main
+    } // fim classe
+} // fim namespace
