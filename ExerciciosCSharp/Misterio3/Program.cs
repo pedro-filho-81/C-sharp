fdff4da591bd5ae990dd9d31a6e13f99a7062519
PedroFilho <psfilho81@hotmail.com>
2021-04-14 10:19:52 -0300

    Mistério 3

---
+++ ExerciciosCSharp/Misterio3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Misterio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int linha = 10;
+            while( linha >= 1 )
+            {
+                int coluna = 1;
+                while( coluna <= 10 )
+                {
+                    System.Console.WriteLine(linha % 2 == 1 ? "<" : ">" );
+                }
+            }
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+    }
+}
