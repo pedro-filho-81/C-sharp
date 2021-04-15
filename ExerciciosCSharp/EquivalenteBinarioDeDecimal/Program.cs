9ad138d3a574000905ef70d02c650fee8ca21417
PedroFilho <psfilho81@hotmail.com>
2021-04-15 10:24:41 -0300

    Converte Decimal Em Binário

---
+++ ExerciciosCSharp/EquivalenteBinarioDeDecimal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EquivalenteBinarioDeDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numDecimal = 0;
+            int contador = 1;
+
+            // entre com um inteiro
+            System.Console.Write("Entre com um número inteiro: ");
+            numDecimal = int.Parse(Console.ReadLine());
+
+            while( numDecimal / 2 != 0 )
+            {
+                //numDecimal %= 2;
+                System.Console.WriteLine($"{numDecimal}");
+                //numDecimal /= 2;
+
+                contador++;
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
