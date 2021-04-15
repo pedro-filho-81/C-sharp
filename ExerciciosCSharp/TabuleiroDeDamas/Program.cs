5da64f4b777f6906f30b88bede1fb8c7f7a56368
PedroFilho <psfilho81@hotmail.com>
2021-04-15 17:26:11 -0300

    Tabuleiro de Dama com Astreisco

---
+++ ExerciciosCSharp/TabuleiroDeDamas/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TabuleiroDeDamas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variável
+            int linha = 0;
+            int coluna = 0;
+
+            // enquanto linha menor que 10
+            while( linha < 10 )
+            {
+                // enquanto coluna menor que 10
+                while( coluna < 10 )
+                {
+                    // imprima
+                    System.Console.Write("* ");
+                    coluna++; // incrementa 1
+                }
+
+                System.Console.WriteLine();
+
+                if( linha % 2 == 0 )
+                {
+                    System.Console.Write(" ");
+                }
+                
+                linha++;
+                coluna = 0;
+            }
+        }
+    }
+}
