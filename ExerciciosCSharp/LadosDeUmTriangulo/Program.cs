f8e2d03dc5017b33d6e5b31f6cdae742d73419a9
PedroFilho <psfilho81@hotmail.com>
2021-04-16 09:58:09 -0300

    Pode Ser Um Triângulo?

---
+++ ExerciciosCSharp/LadosDeUmTriangulo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace LadosDeUmTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variáveis
+            int ladoA, ladoB, ladoC;
+            int sentinela = 1;
+
+            // enquando sentinela diferente e 1 faça
+            while( sentinela != -1 )
+            {       
+                Console.Clear(); // limpa a tela
+
+                // entrada de dados
+                Console.Write("Digite o ladoA do triângulo: " );
+                ladoA = int.Parse(Console.ReadLine());
+                
+                // entrada de dados
+                Console.Write("Digite o ladoB do triângulo: " );
+                ladoB = int.Parse(Console.ReadLine());
+                
+                // entrada de dados
+                Console.Write("Digite o ladoC do triângulo: " );
+                ladoC = int.Parse(Console.ReadLine());
+                
+                // se a menor que b mais c b < c + a e c < a + b
+                if( ladoA < ladoB + ladoC && ladoB < ladoA + ladoC && 
+                    ladoC < ladoA + ladoB) // se verdade
+                {
+                    // imprima
+                    Console.WriteLine($"A = {ladoA}, B = {ladoB}, C = {ladoC} " +
+                        $"\nPode ser um triângolo.");
+                } // fim if
+                else // se não
+                {
+                    // imprima
+                    Console.WriteLine($"A = {ladoA}, B = {ladoB}, C = {ladoC} " +
+                        $"\nNão pode ser um triângolo.");
+                } // fim else
+
+                System.Console.Write("Deseja continuar? [ 1=sim / -1=não ]");
+                sentinela = int.Parse(Console.ReadLine());
+
+            } // fim while
+
+            // limpa a tela
+            Console.Clear();
+        } // fim main
+    } // fim classe
+} // fim namespace
