fc2b22b9f40b5e46b6916cac318365478804355e
PedroFilho <psfilho81@hotmail.com>
2021-04-18 11:01:40 -0300

    Notas dos Alunos

---
+++ ExerciciosCSharp/NotaDosAlunos/Program.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace NotaDosAlunos
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            // cria variáveis
+            int somaNotas = 0;
+            int contaNotas = 0;
+            int notaA = 0;
+            int notaB = 0;
+            int notaC = 0;
+            int notaD = 0;
+            int notaE = 0;
+            int notaF = 0;
+            
+            Console.WriteLine("Digite as notas entre 0 - 100: ");
+
+        }
+    }
+}
