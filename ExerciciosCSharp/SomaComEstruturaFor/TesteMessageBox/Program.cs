236831bc622ce7d2440d88db8dfcd1f7629840da
PedroFilho <psfilho81@hotmail.com>
2021-04-21 10:25:50 -0300

    Diversos

---
+++ ExerciciosCSharp/SomaComEstruturaFor/TesteMessageBox/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace TesteMessageBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
