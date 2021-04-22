ea6cbf725e4d9f64800afb8c2daee2cf5bdc9658
PedroFilho <psfilho81@hotmail.com>
2021-04-22 11:00:30 -0300

    Atualização

--- Capitulo05_CSharpDeitel/ProdutoDosInteirosImpares/Program.cs
+++ Capitulo05_CSharpDeitel/ProdutoDosInteirosImpares/Program.cs
@@ -5,11 +5,29 @@ namespace ProdutoDosInteirosImpares
     class Program
     {
         /*
-            
+            5.12 (Calculando o produto de números inteiros ímpares) Escreva um aplicativo 
+            que calcula o produto dos números inteiros ímpares de 1 a 15. 
+            Deitel, Paul; Deitel, Harvey. Java: como programar (p. 154). Edição do Kindle. 
+            Autor: Pedro Filho, 22/04/2021
         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // cria variável
+            int produto = 1;
+
+            // loop para contar de 1 a 15
+            for( int i = 1; i <= 15; i++ )
+                // se i % 2 == 1 ( se i for ímpar )
+                if( i % 2 == 1 )
+
+                // calcular o valor
+                produto *= i;
+
+            // imprima o resultado    
+            Console.WriteLine( $"O produto dos ímpares de 1 a 15 é {produto}" );
+            Console.ReadKey();
+            Console.Clear();
+            
         }
     }
 }
