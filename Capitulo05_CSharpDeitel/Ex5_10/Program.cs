826c9415f86c7c7b7fd5bc44b0d32c2cd5936583
PedroFilho <psfilho81@hotmail.com>
2021-04-22 10:19:17 -0300

    Mistério1

---
+++ Capitulo05_CSharpDeitel/Ex5_10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex5_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <=10; i++ )
+            {
+                for( int j = 1; j <= 5; j++ )
+                    System.Console.WriteLine('@');
+
+                System.Console.WriteLine();
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
