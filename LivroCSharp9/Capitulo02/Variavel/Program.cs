fa05b021554fd47bc396ab8cbbfe42d34c74d523
PedroFilho <psfilho81@hotmail.com>
2021-04-24 17:39:24 -0300

    Variáveis Object e Dynamic

---
+++ LivroCSharp9/Capitulo02/Variavel/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace Variavel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clear(); // limpa a tela
+
+            object altura = 1.81; // atribuindo um double a um objeto
+            object nome = "Pedro"; // atribuindo uma string a um objeto
+            //monsta o conteúdo dos objetos
+            Console.WriteLine($"{nome} tem {altura}m de altura");
+
+            //int tamanho = nome.Length; // vai dá erro, tem que usar o Typecast
+            int tamanhoDoNome = ( (string) nome ).Length; // mostra o tamanho da string
+            // imprima
+            WriteLine($"{nome} tem {tamanhoDoNome} letras." );
+
+            dynamic outroNome = "Maria"; // atribuindo string a dynamic
+            int tamanho2 = outroNome.Length; // mostra o tamanho do outro nome
+            // imprime
+            WriteLine($"{outroNome} tem {tamanho2} caracteres." );
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
