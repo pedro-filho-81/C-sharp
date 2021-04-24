5dce79fd4430bd515abd0414bd2c706065e816d2
PedroFilho <psfilho81@hotmail.com>
2021-04-24 17:39:54 -0300

    Argumentos

---
+++ CSharpMetodos/Argumentos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clear();
+            
+            Console.WriteLine($"Existe {args.Length} argumentos");
+
+            foreach(string arg in args )
+            {
+                WriteLine(arg);
+            }
+        }
+    }
+}
