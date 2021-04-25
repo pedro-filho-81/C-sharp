b39084932345268c0c984bc89fbf0b91c209d0ce
PedroFilho <psfilho81@hotmail.com>
2021-04-25 11:31:50 -0300

    Formatação

---
+++ LivroCSharp9/Capitulo02/Formatando/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Formatando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Console.Clear();
+            
+            // cria variáveis
+            int numeroDeMacaes = 12;
+            decimal precoPorMacaes = 0.35M;
+            // imprime
+            Console.WriteLine(format:"{0} maçães consta {1:C}", arg0: numeroDeMacaes, arg1: precoPorMacaes *
+                numeroDeMacaes );
+
+            // cria variável e atribui valor
+            string formatado = string.Format( format: "{0} maçães custam {1:C}", arg0: numeroDeMacaes, 
+                arg1: precoPorMacaes * numeroDeMacaes );
+            // imprime a variável
+            Console.WriteLine($"{formatado} Hello World!");
+        } // fim main
+    } // fim classe
+} // fim namespace
