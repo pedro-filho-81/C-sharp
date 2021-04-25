b361581acef93332e7831feb55cdf34cd45a0ab0
PedroFilho <psfilho81@hotmail.com>
2021-04-25 10:53:21 -0300

    Variável Nula

---
+++ LivroCSharp9/Capitulo02/VariavelNula/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VariavelNula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // cria variáveis
+            // int esteNaoPodeSerNulo = null;
+            // atribuindo valor
+            // esteNaoPodeSerNulo = 4;
+            // mostra
+            // .Console.WriteLine($"Este não pode ser nulo {esteNaoPodeSerNulo}");
+
+            // este pode ser nulo
+            int? estePodeSerNulo = null;
+            Console.WriteLine($"Este pode ser nulo {estePodeSerNulo}");
+
+            // atribuindo valores
+            estePodeSerNulo = 7;
+            Console.WriteLine($"Este pode ser nulo vale {estePodeSerNulo}");
+            Console.WriteLine($"Este pode ser nulo {estePodeSerNulo.GetValueOrDefault()}");
+        } // fim main
+    } // fim classe
+} // fim namespace
