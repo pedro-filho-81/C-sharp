d71fe2c04a79ed1fbe4259011808331330f7d6ed
PedroFilho <psfilho81@hotmail.com>
2021-04-25 10:36:12 -0300

    Array Simples

---
+++ LivroCSharp9/Capitulo02/Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // cria um array
+            string[] nomes; // pode referenciar qualquer array de string
+            
+            // alocando memoria para 4 string em um array
+            nomes = new string[ 4 ];
+            
+            // limpa a tela
+            Console.Clear();
+            
+            // indexando valores para o array
+            nomes[0] = "Pedro";
+            nomes[1] = "Paulo";
+            nomes[2] = "Maria";
+            nomes[3] = "José";
+            
+            // loop para mostrar os nomes
+            for( int i = 0; i < nomes.Length; i++ )
+            {
+                System.Console.WriteLine($"Nomes[{i}] = {nomes[i]}");
+            } // fim for
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
