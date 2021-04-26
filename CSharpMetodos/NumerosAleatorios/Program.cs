12090bffd5ba67c96eec6273f38b3558531ec899
PedroFilho <psfilho81@hotmail.com>
2021-04-26 10:18:32 -0300

    Atualização

--- CSharpMetodos/NumerosAleatorios/Program.cs
+++ CSharpMetodos/NumerosAleatorios/Program.cs
@@ -6,25 +6,34 @@ namespace NumerosAleatorios
     {
         static void Main(string[] args)
         {    
-            // cria o objeto para números aleatórios
+            // 1º) cria o objeto para números aleatórios
             Random numero = new Random();
 
             // limpa a tela
             Console.Clear();
 
+            // cabeçalho
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            System.Console.WriteLine("NÚMEROS ALEATÓRIOS");
+            Console.ResetColor();
+
             // loop para imprimir 20 números aleatórios
             for( int i = 1; i <= 20; i++ )
             {
-                // cria variável que recebe números entre 1 e 6 aleatórios
+                // 2º) cria variável que recebe números aleatórios entre 1 e 6
+                // 3º) use o objeto.Next(número); ou objeto.Next( valor inicial, 
+                                                // valor final maior que o desejado );
                 int numeroAleatorio = numero.Next( 1, 7 );
 
                 // imprime os números
-                Console.Write($"{numeroAleatorio} " );
+                Console.Write($" {numeroAleatorio} " );
 
                 // quando i multiplo de 5
                 if( i % 5 == 0 )
                     // pule uma linha
                     System.Console.WriteLine();
+
             } // fim for
             
         } // fim main
