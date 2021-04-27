564d2056d37506126e6c5323301fa7266dccb2e0
PedroFilho <psfilho81@hotmail.com>
2021-04-27 11:02:46 -0300

    Método Math

---
+++ CSharpMetodos/TestandoMath/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+namespace TestandoMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clear(); // limpa a tela
+
+            // cabeçalho
+            BackgroundColor = ConsoleColor.DarkBlue;
+            ForegroundColor = ConsoleColor.White;
+            WriteLine("MÉTODO MATH");
+            ResetColor();
+
+            WriteLine($"Math.Abs(23.7) = {Math.Abs(23.7)}");
+            WriteLine($"Math.Abs(-23.7) = {Math.Abs(-23.7)}");
+            WriteLine($"Math.Abs(0.0) = {Math.Abs(0.0)}");
+
+            WriteLine($"Math.Ceiling(9.2) = {Math.Ceiling(9.2)}");
+            WriteLine($"Math.Ceiling(-9.8) = {Math.Ceiling(-9.8)}");
+
+            WriteLine($"Math.Cos(0.0) = {Math.Cos(0.0)}");
+
+            WriteLine($"Math.Exp(1.0) = {Math.Exp(1.0)}");
+            WriteLine($"Math.Exp(2.0) = {Math.Exp(2.0)}");
+
+            WriteLine($"Math.Floor(9.2) = {Math.Floor(9.2)}");
+            WriteLine($"Math.Floor(-9.8) = {Math.Floor(-9.8)}");
+
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
