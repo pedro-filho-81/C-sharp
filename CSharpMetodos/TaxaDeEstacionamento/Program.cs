8f605b35a5391ade415fb1e78c41ae97e5d9f349
PedroFilho <psfilho81@hotmail.com>
2021-04-29 17:36:12 -0300

    Tarifa de Estacionamento

---
+++ CSharpMetodos/TaxaDeEstacionamento/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using static System.Console;
+
+namespace TaxaDeEstacionamento
+{
+    class Program
+    {
+        /*
+            6.8 (Taxas de estacionamento) Um estacionamento cobra uma tarifa mínima de 
+            R$ 2,00 para estacionar por até três horas. Um adicional  de R$ 0,50 por hora 
+            não necessariamente inteira é cobrado após as três primeiras horas. A tarifa 
+            máxima para qualquer dado período  de 24 horas é R$ 10,00. Suponha que nenhum 
+            carro fique estacionado por mais de 24 horas por vez. Escreva um aplicativo que 
+            calcule e  exiba as tarifas de estacionamento para cada cliente que estacionou 
+            nessa garagem ontem. Você deve inserir as horas de estacionamento  para cada cliente. 
+            O programa deve exibir a cobrança para o cliente atual e calcular e exibir 
+            o total dos recibos de ontem. Ele deve utilizar  o método calculateCharges para 
+            determinar a tarifa para cada cliente. 
+            Deitel, Paul; Deitel, Harvey. Java: como programar (p. 186). Edição do Kindle. 
+            Autor: Pedro Filho, 29/04/2021
+        */
+
+        static void Main(string[] args)
+        {
+            // cria variável
+            float tempo = 0;
+            decimal resposta = 0M;
+
+            Clear(); // limpa tela
+
+            // cabeçalho
+            BackgroundColor = ConsoleColor.DarkBlue;
+            ForegroundColor = ConsoleColor.White;
+            WriteLine("TAXA DE ESTACIONAMENTO");
+            ResetColor();
+
+            // entrada de dados
+            Write("Digite o tempo que o carro ficou estacionado:" );
+            tempo = float.Parse(ReadLine());
+
+            // resposta recebe o valor retornado do método 
+            resposta = CalTaxaDeEstacionamento( tempo );
+
+            // imprime resuktado
+            Console.WriteLine($"Por {tempo}h estacionado valor a pagar { resposta:C}");
+        } // fim main
+
+        public static decimal CalTaxaDeEstacionamento( float tempoEstacionado ) 
+        {
+            // cria variável
+            decimal valorDaTarifa = 0M;
+            float tempoExtra = 0.0F;
+
+            // até 3 horas estacionado
+            if(tempoEstacionado > 0.0F && tempoEstacionado <= 3.0F )
+                // o valor da tarifa é de 
+                valorDaTarifa = 2.0M;
+
+            // acima de 3 horas estacionado
+            else if( tempoEstacionado > 3.0f && tempoEstacionado < 24.0f )
+                // calcular o tempo extra
+                tempoExtra = tempoEstacionado - 3.0f;
+                // e o valor da tarifa é calculado com o tempo extra
+                valorDaTarifa = (decimal) tempoExtra * 0.5M + 2.0M;
+
+            return valorDaTarifa; // retorna o valor da tarifa
+
+        } // fim método
+        
+    } // fim classe
+} // fim namespace
