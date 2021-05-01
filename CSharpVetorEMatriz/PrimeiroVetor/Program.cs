75b6c81a565beaf2154afb428a2c5280319195a8
PedroFilho <psfilho81@hotmail.com>
2021-05-01 17:29:20 -0300

    Primeiro Vetor CSharp

---
+++ CSharpVetorEMatriz/PrimeiroVetor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrimeiroVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // cria variável para vetor
+            int[] vetor = new int[10];
+
+            // limpa a tela
+            Console.Clear();
+
+            // imprime 
+            Console.WriteLine($"  Índice {"Valor",10}");
+
+            // loop for para adicionar valores ao vetor
+            for( int contar = 0; contar < vetor.Length; contar++ )
+            {
+                // imprima o índice e o valor (0) adicionado a cada índice
+                Console.WriteLine($"vetor[{contar}] {vetor[contar], 10}");
+            } // fim for
+
+        } // fim main
+    } // fim classe
+} // fim namespace
