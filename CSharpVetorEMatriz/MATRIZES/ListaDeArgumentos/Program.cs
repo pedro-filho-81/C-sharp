0eef70be48942829845989fd5fba1474c7c0ede5
PedroFilho <psfilho81@hotmail.com>
2021-05-12 16:32:18 -0300

    Lista de Argumentos

---
+++ CSharpVetorEMatriz/MATRIZES/ListaDeArgumentos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace ListaDeArgumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            Console.WriteLine("Hello World!");
+        }
+
+        static double Media(params double[] numeros )
+        {
+            // cria variável
+            var total = 0.0;
+
+            // cria um loop para percorrer o vetor
+            foreach( var valor in numeros )
+            {
+                // variável total soma os valores do vetor números 
+                total += valor;
+            } // fim foreach
+        } // fim função
+        
+    }
+}
