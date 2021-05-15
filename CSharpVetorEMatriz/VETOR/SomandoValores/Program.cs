7cac7677260da9e527029e03bf5739aa21cc79ad
PedroFilho <psfilho81@hotmail.com>
2021-05-15 16:20:03 -0300

    Somando os Elementos do Vetor

---
+++ CSharpVetorEMatriz/VETOR/SomandoValores/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SomandoValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
