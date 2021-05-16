6571f2e1d8a46b97bb37348b189acef0e59a9d6b
PedroFilho <psfilho81@hotmail.com>
2021-05-16 10:16:28 -0300

    Quarto vetor

---
+++ CSharpVetorEMatriz/VETOR/QuartoVetor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+
+namespace QuartoVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // cria uma constante
+            const int TAMANHO = 10;
+            //  cria o vetor
+            int[] vetor = new int[TAMANHO];
+            // cria variável
+            int soma = 0;
+
+            // limpa tela
+            Clear();
+
+            // loop para adicionar valores ao vetor
+            for( int adicionar = 0; adicionar < TAMANHO; adicionar++ )
+            {
+                // adiciona valores ao vetor
+                vetor[ adicionar ] = adicionar * 3;
+                // soma os valores do vetor
+                soma += vetor[ adicionar ];
+
+            } // fim for adicionar
+
+            // cabeçalho
+            WriteLine( $"Índice {"Valor", 5}" );
+
+            // loop mostrar valores do vetor
+            for( int mostrar = 0; mostrar < TAMANHO; mostrar++ )
+            {
+                // imprime
+                WriteLine( $" {mostrar + 1, 3} {vetor[ mostrar ], 5}" );
+            } // fim for mostrar
+
+            // imprime
+            Console.WriteLine( $"Soma = {soma}");
+        } // fim main
+
+    } // fim classe
+} // fim namespace
