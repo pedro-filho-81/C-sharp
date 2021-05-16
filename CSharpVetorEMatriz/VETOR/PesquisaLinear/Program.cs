1f22cc335cd100d86daf84a2b1a323f1eee70b8d
PedroFilho <psfilho81@hotmail.com>
2021-05-16 15:43:35 -0300

    Pesquisando no Vetor

---
+++ CSharpVetorEMatriz/VETOR/PesquisaLinear/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PesquisaLinear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
