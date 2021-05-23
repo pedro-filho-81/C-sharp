d5f8b08689172324ecaf2a32d6ca59235bbf9f19
PedroFilho <psfilho81@hotmail.com>
2021-05-23 18:22:06 -0300

    Foreach

---
+++ CSharpVetorEMatriz/MATRIZES/ForEach/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+
+namespace ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa tela
+            Clear();
+
+            // cria vetor
+            var d1 = 10;
+            var d2 = 20;
+            var d3 = 30;
+            var d4 = 40;
+            
+            // imprima
+            WriteLine( $"d1 = {d1:f1}\nd2 = {d2:f1}\nd3 = {d3:f1}\nd4 = {d4:f1}");
+
+            // imprima
+            WriteLine($"A média entre d1 e d2 é {Media(d1, d2):f1}" );
+            WriteLine( $"A média entre d1, d2, d3 é {Media(d1, d2, d3):f1}" );
+            
+
+        } // fim main
+
+        // cria a função média
+        static double Media( params double[] numeros )
+        {
+            // cria variável
+            var total = 0.0;
+
+            // loop para percorrer o vetor
+            foreach( var numero in numeros )
+            {
+                total += numero;
+            } // fim foreach
+
+            // se o vetor diferente de zero
+            return numeros.Length != 0 ? total / numeros.Length:0.0;
+
+        } // fim media
+
+    } // fim classe
+} // fim namespace
