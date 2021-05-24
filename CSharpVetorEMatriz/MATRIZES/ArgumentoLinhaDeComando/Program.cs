a15160bbe49666671ad54cdd6217e2f41c18da9e
PedroFilho <psfilho81@hotmail.com>
2021-05-24 09:37:40 -0300

    Argumento na Linha de Comando

---
+++ CSharpVetorEMatriz/MATRIZES/ArgumentoLinhaDeComando/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArgumentoLinhaDeComando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
