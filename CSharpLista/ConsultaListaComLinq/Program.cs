41254ef72d9025a1970d52d29345017ee57b6ee1
PedroFilho <psfilho81@hotmail.com>
2021-05-26 10:15:42 -0300

    Consulta Lista com LINQ

---
+++ CSharpLista/ConsultaListaComLinq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace ConsultaListaComLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
