ca3a9152a99ede77db91246ab4a9ffe0bc00dcb8
PedroFilho <psfilho81@hotmail.com>
2021-05-26 19:56:00 -0300

    Testando O Tempo

---
+++ CSharpClasses/TestandoTempo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+using System.Collections.Generic;
+
+namespace TestandoTempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
