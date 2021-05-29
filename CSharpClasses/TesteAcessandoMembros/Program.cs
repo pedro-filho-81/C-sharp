79427f73e8cf5ae3647b0889334b5df1a03e19dd
PedroFilho <psfilho81@hotmail.com>
2021-05-29 10:14:19 -0300

    Teste Acessando Membros

---
+++ CSharpClasses/TesteAcessandoMembros/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace TesteAcessandoMembros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa tela
+            Clear();
+            var hora = new Tempo1();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
