e2c21aecdbf5210557c263fa75eb2672f6f45433
PedroFilho <psfilho81@hotmail.com>
2021-05-29 17:29:04 -0300

    Testando this

---
+++ CSharpClasses/TestandoThis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace TestandoThis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            // cria um objeto da classe simples tempo
+            var tempo = new SimplesTempo(13, 25, 35 );
+            WriteLine($"{tempo.ConstruirString()}" );
+        
+        } // fim main
+
+    } // fim classe
+} // fim namespace
