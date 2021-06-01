79ac552888dd18d79d091dbf23d18a1821fe61a7
PedroFilho <psfilho81@hotmail.com>
2021-06-01 12:00:09 -0300

    Conta Bancaria 4

---
+++ LivroC#ComoProgramar/Capitulo4/Exemplos/ContaBancaria4/Conta.cs
@@ -0,0 +1,51 @@
+using System;
+
+class Conta
+{
+    // cria a propriedade nome
+    public string Nome { get; set; }
+    // cria a variável privada saldo
+    private decimal saldoInicial;
+
+    // cria o construtor da classe
+    public Conta( string nome, decimal saldoInicial )
+    {
+        // propriedade nome recebe variável
+        Nome = nome;
+        Saldo = saldoInicial;
+
+    } // fim construtor
+
+    // cria a propriedade Saldo
+    public decimal Saldo
+    {
+        get
+        {
+            // retorna o valor do saldo inicial
+            return saldoInicial;
+        } // fim get
+        set
+        {
+            // se o valor for maior que zero
+            if( value > 0.0m )
+            {
+                // saldo inicial recebe o valor
+                saldoInicial = value;
+            } // fim if
+
+        } // fim set
+    } // fim propriedade
+
+    // cria o método depósito
+    public void Deposito( decimal valorDoDeposito )
+    {
+        // se o valor do depósito for maior que zero
+        if( valorDoDeposito > 0.0m )
+        {
+            // Saldo soma mais valor do depósito
+            Saldo += valorDoDeposito; 
+        } // fim if
+
+    } // fim método depósito
+
+} // fim classe
---
+++ LivroC#ComoProgramar/Capitulo4/Exemplos/ContaBancaria4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace ContaBancaria4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            // chama a classe Conta e atribui valor
+            Conta minhaConta = new Conta("Pedro", 1000m);
+            Conta minhaConta1 = new Conta("Cleonice", 500m);
+            Conta minhaConta2 = new Conta("Tatiane", 250.25m);
+
+            // cabeçalho
+            WriteLine("LISTA DOS CORRENTISTAS DO BANCO COM SALDO INICIAL" );
+
+            // IMPRIME RESULTADO
+            WriteLine($"A conta 1 é de {minhaConta.Nome} saldo inicial de {minhaConta.Saldo:C}");
+            WriteLine($"A conta 2 é de {minhaConta1.Nome} saldo inicial de {minhaConta1.Saldo:c}");
+            WriteLine($"A conta 3 é de {minhaConta2.Nome} saldo inicial de {minhaConta2.Saldo:C}\n");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
