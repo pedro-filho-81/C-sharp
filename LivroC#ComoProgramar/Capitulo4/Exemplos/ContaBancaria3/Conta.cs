49372ebcbe517fef1f5a3f9e06ae6de2e5fceb9d
PedroFilho <psfilho81@hotmail.com>
2021-06-01 11:11:39 -0300

    Conta Bancária 3

---
+++ LivroC#ComoProgramar/Capitulo4/Exemplos/ContaBancaria3/Conta.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Conta
+{
+    public string Nome { get; set; }
+
+    // cria o construtor da classe
+    public Conta( string nome )
+    {
+        // a propriedade Nome recebe a variável nome
+        Nome = nome;
+        
+    } // fim construtor
+} // fim da classe conta
---
+++ LivroC#ComoProgramar/Capitulo4/Exemplos/ContaBancaria3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace ContaBancaria3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            // chama a classe conta e cria o objeto minhaConta e atribui um valor
+            Conta minhaConta = new Conta( "Pedro" );
+            // cria novo objeto
+            Conta minhaConta1 = new Conta("Maria");
+            // cria novo objeto
+            Conta minhaConta2 = new Conta("Cleonice");
+
+            // cabeçalho
+            WriteLine("LISTA DOS NOMES DAS CONTAS:");
+            // chama a classe Conta através do objeto minhaConta e mostra o nome armazenado
+            WriteLine($"Primeiro nome da conta é {minhaConta.Nome}");
+            WriteLine($"Segundo nome da conta é {minhaConta1.Nome}");
+            WriteLine($"Terceiro nome da conta é {minhaConta2.Nome}");
+        } // fim main
+    } // fim classe principal
+} // fim namespace
