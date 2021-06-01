695169a9219556cb1c92a7eb97794e61336e5396
PedroFilho <psfilho81@hotmail.com>
2021-06-01 09:42:58 -0300

    Conta Bancária 1

---
+++ LivroC#ComoProgramar/Capitulo4/Exemplos/ContaBancaria1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            // cria o objeto da classe conta
+            Conta minhaConta = new Conta();
+
+            // mostra o nome inicial
+            WriteLine($"O nome da conta inicial: {minhaConta.GetNome()}");
+
+            // entrada de dados
+            Write("Digite o nome: " );
+            // cria a variável nome e aguarda a tetrada pelo usuário
+            string nome = ReadLine();
+            // o objeto minha conta chama o método set nome e atribui o nome digitado
+            minhaConta.SetNome(nome);
+
+            // o nome da conta é
+            WriteLine($"Nome da conta: {minhaConta.GetNome()}");
+
+        } // fim main
+    } // fim classe
+} // fim namespace
