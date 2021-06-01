e1b774a0fe57e0f76a5e3fcf89682f1f59b60813
PedroFilho <psfilho81@hotmail.com>
2021-06-01 09:32:12 -0300

    Classe Data

---
+++ CSharpClasses/Data/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
