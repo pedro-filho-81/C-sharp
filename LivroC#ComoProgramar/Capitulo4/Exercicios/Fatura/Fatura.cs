0c89f0140a0946d7e191ae1bd08c2d667175b955
PedroFilho <psfilho81@hotmail.com>
2021-06-04 17:28:57 -0300

    Fatura

---
+++ LivroC#ComoProgramar/Capitulo4/Exercicios/Fatura/Fatura.cs
@@ -0,0 +1,6 @@
+using System;
+
+class Fatura
+{
+    
+}
---
+++ LivroC#ComoProgramar/Capitulo4/Exercicios/Fatura/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace Fatura
+{
+    class Program
+    {
+        /*
+            3.12 (Classe Invoice) Crie uma classe chamada Invoice para que uma loja de 
+            suprimentos de informática a utilize para representar uma  fatura de um item 
+            vendido nela. Uma Invoice (fatura) deve incluir quatro partes das informações 
+            como variáveis de instância — o  número (tipo String), a descrição (tipo String), 
+            a quantidade comprada de um item (tipo int) e o preço por item (double). 
+            Sua  classe deve ter um construtor que inicializa as quatro variáveis de instância. 
+            Forneça um método set e um get para cada variável de instância. Além disso, forneça 
+            um método chamado getInvoiceAmount que calcula o valor de fatura 
+            (isto é, multiplica a quantidade pelo  preço por item) e depois retorna esse valor 
+            como double. Se a quantidade não for positiva, ela deve ser configurada como 0. 
+            Se o preço  por item não for positivo, ele deve ser configurado como 0.0. 
+            Escreva um aplicativo de teste chamado InvoiceTest que demonstra as  capacidades 
+            da classe Invoice.  3.13 (Classe Employee) Crie
+            Deitel, Paul; Deitel, Harvey. Java: como programar (p. 77). Edição do Kindle. 
+            Autor: Pedro Filho, 04/06/2021
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
