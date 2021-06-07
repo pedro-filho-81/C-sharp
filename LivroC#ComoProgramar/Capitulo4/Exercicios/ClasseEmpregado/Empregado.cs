a987341ed1b058676480f6cfb3c09246e73e41e4
PedroFilho <psfilho81@hotmail.com>
2021-06-07 11:15:49 -0300

    Classe Empregado

---
+++ LivroC#ComoProgramar/Capitulo4/Exercicios/ClasseEmpregado/Empregado.cs
@@ -0,0 +1,34 @@
+using System;
+
+class Empregado
+{
+    // cria variáveis de instância
+    public string primeiroNome;
+    public string ultimoNome;
+    private decimal salarioMensal;
+
+    // cria o construtor
+    public Empregado( string primeiroNome, string ultimoNome, decimal salarioMensal )
+    {
+        this.primeiroNome = primeiroNome;
+        this.ultimoNome = ultimoNome;
+        SalarioMensal = salarioMensal;
+    } // fim construtor
+
+    // cria propriedade Salário mensal
+    public decimal SalarioMensal
+    {
+        get
+        {
+            return salarioMensal;
+        } // fim get
+    
+    } // fim propriedade
+
+    // cria método salário anual
+    public decimal SalarioAnual( decimal salarioMensal )
+    {
+        return salarioMensal * 12;
+    } // fim método salário anual
+
+} // fim classe empregado
---
+++ LivroC#ComoProgramar/Capitulo4/Exercicios/ClasseEmpregado/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace ClasseEmpregado
+{
+    class Program
+    {
+        /*
+            3.13 (Classe Employee) Crie uma classe chamada Employee que inclua três variáveis 
+            de instância — um primeiro nome (tipo String), um  sobrenome (tipo String) e um 
+            salário mensal (double). Forneça um construtor que inicializa as três variáveis 
+            de instância. Forneça um  método set e um get para cada variável de instância. 
+            Se o salário mensal não for positivo, não configure seu valor. Escreva um aplicativo 
+            de  teste chamado EmployeeTest que demonstre as capacidades da classe Employee. 
+            Crie dois objetos Employee e exiba o salário anual  de cada objeto. Então dê a cada 
+            Employee um aumento de 10% e exiba novamente o salário anual de cada Employee. 
+            Deitel, Paul; Deitel, Harvey. Java: como programar (p. 77). Edição do Kindle. 
+            Autor: Pedro Filho, 07/06/2021.
+        */
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
