3482e876545b7fb9d52402b3243c96cf94a14772
PedroFilho <psfilho81@hotmail.com>
2021-06-08 17:04:58 -0300

    Removendo C´ódigo Duplicado

---
+++ LivroC#ComoProgramar/Capitulo4/Exercicios/RemovendoCodigo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace RemovendoCodigo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            Console.WriteLine("Hello World!");
+        } // fim main
+    } // fim class
+} // fim namespace
