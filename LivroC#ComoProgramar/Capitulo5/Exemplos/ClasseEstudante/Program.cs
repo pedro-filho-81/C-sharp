d3c95ba97cba85d282f3e443a5d5a6a7a6fc855a
PedroFilho <psfilho81@hotmail.com>
2021-06-10 10:14:35 -0300

    Atualização

--- LivroC#ComoProgramar/Capitulo5/Exemplos/ClasseEstudante/Program.cs
+++ LivroC#ComoProgramar/Capitulo5/Exemplos/ClasseEstudante/Program.cs
@@ -14,6 +14,12 @@ namespace ClasseEstudante
             Estudante mediaDoAluno = new Estudante("Pedro", 95 );
             // chama o método mostra a média
             MostraMedia( mediaDoAluno );
+            Estudante mediaDoAluno1 = new Estudante("Cleonice", 90 );
+            MostraMedia(mediaDoAluno1);
+
+            Estudante mediaDoAluno2 = new Estudante("Maria", 70 );
+            MostraMedia(mediaDoAluno2);
+
 
             Console.WriteLine("Hello World!");
         } // fim main
@@ -22,7 +28,7 @@ namespace ClasseEstudante
         public static void MostraMedia( Estudante minhaMedia )
         {
             // mosta a média
-            WriteLine($"{minhaMedia.Nome} média {minhaMedia.Media}" );
+            WriteLine($"{minhaMedia.Nome} média {minhaMedia.Media} nota {minhaMedia.LetraDaNota}" );
         } // fim método mostra a media
 
     } // fim classe
