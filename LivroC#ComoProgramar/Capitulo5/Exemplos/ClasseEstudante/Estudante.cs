f750780738103d042a53b78fad483bc7a830aa22
PedroFilho <psfilho81@hotmail.com>
2021-06-10 10:04:57 -0300

    Classe Estudante

---
+++ LivroC#ComoProgramar/Capitulo5/Exemplos/ClasseEstudante/Estudante.cs
@@ -0,0 +1,70 @@
+using System;
+
+class Estudante
+{
+    // cria propriedade nome
+    public string Nome { get; set; }
+    // cria variável de instância média
+    private double media;
+
+    // cria o construtor da classe
+    public Estudante( string nome, double media )
+    {
+        Nome = nome;
+        Media = media;
+
+    } // fim construtor 
+
+    // cria propriedade média
+    public double Media
+    {
+        get
+        {
+            return media;
+        } // fim get
+        set
+        {
+            if( value > 0)
+            {
+                if( value <= 100 )
+                {
+                    media = value;
+                } // fim if interno
+
+            } // fim if externo
+        } // fim set
+    } // fim propriedade média
+
+    // cria letra da nota
+    public string LetraDaNota
+    {
+        get
+        {
+            // string vazia
+            string LetraDaNota = string.Empty;
+            if(media >= 90)
+            {
+                LetraDaNota = "A";
+            }
+            else if( media >= 80)
+            {
+                LetraDaNota = "B";
+            }
+            else if( media >= 70 )
+            {
+                LetraDaNota = "C";
+            }
+            else if( media >= 60 )
+            {
+                LetraDaNota = "D";
+            }
+            else
+            {
+                LetraDaNota = "F";    
+            }
+
+            // retorne a letra da nota
+            return LetraDaNota;
+        } // fim get
+    } // fim propriedade letra da nota
+} // fim da classe estudante
---
+++ LivroC#ComoProgramar/Capitulo5/Exemplos/ClasseEstudante/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace ClasseEstudante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            // cria objeto da classe Estudante
+            Estudante mediaDoAluno = new Estudante("Pedro", 95 );
+            // chama o método mostra a média
+            MostraMedia( mediaDoAluno );
+
+            Console.WriteLine("Hello World!");
+        } // fim main
+
+        // cria o método mostra media
+        public static void MostraMedia( Estudante minhaMedia )
+        {
+            // mosta a média
+            WriteLine($"{minhaMedia.Nome} média {minhaMedia.Media}" );
+        } // fim método mostra a media
+
+    } // fim classe
+} // fim namespace
