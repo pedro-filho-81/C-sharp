5bf5608b1e042835318812854ceb5561dd3a1db0
PedroFilho <psfilho81@hotmail.com>
2021-06-10 17:09:31 -0300

    Incremento

---
+++ LivroC#ComoProgramar/Capitulo5/Exemplos/Incremento/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace Incremento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
