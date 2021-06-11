7324b5288e7b8533cfb556795e01df83f6c79eac
PedroFilho <psfilho81@hotmail.com>
2021-06-11 16:33:29 -0300

    Quilometragem de Combustíveis

---
+++ LivroC#ComoProgramar/Capitulo5/Exercicios/QuilometragemDeCombustiveis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuilometragemDeCombustiveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
