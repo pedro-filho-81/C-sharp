30677f8635634afdccd2ba6fa15cd84b2a1a868a
PedroFilho <psfilho81@hotmail.com>
2021-06-18 20:59:55 -0300

    Limite de Crédito

---
+++ LivroC#ComoProgramar/Capitulo5/Exercicios/LimiteDeCredito/Credito.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Credito
+{
+    // cri variáveis de instância
+    private int numeroDaConta;
+    private int saldoInicial;
+    private int limiteDeCredito;
+    
+} // fim da classe
---
+++ LivroC#ComoProgramar/Capitulo5/Exercicios/LimiteDeCredito/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace LimiteDeCredito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
