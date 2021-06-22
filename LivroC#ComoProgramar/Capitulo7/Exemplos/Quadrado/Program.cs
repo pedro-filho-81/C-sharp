5ebe0534f95ff04e4ad18f7f341382aa93876861
PedroFilho <psfilho81@hotmail.com>
2021-06-22 10:52:32 -0300

    Quadrado do Número

---
+++ LivroC#ComoProgramar/Capitulo7/Exemplos/Quadrado/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Quadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Console.Clear();
+
+            // cria variável
+            int numero = 10;
+
+            Console.WriteLine($"O valor do número {numero} " +
+                            $"no método QuadradoDoNumero é {QuadradoDoNumero(numero)}");
+        } // fim main
+
+        // cria o método QuadradoDoNumero
+        static int QuadradoDoNumero( int numero )
+        {
+            return numero * numero;
+        } // fim do método
+    } // fim da classe
+} // fim do namespace
