4803b632524f39f57796403cb040f9269d938188
PedroFilho <psfilho81@hotmail.com>
2021-06-22 11:34:40 -0300

    Potência do Número

---
+++ LivroC#ComoProgramar/Capitulo7/Exemplos/Potencia/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+
+namespace Potencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            // mostrar
+            WriteLine($"Potência(10) = {Potencia(10)}");
+
+            WriteLine($"Potência(2, 10) = {Potencia(2, 10)}");
+
+            Console.WriteLine("Hello World!");
+        } // fim main
+
+        // cria o método Potencia
+        static int Potencia( int valorBase, int expoente = 2 )
+        {
+            int resultado = 1;
+
+            // loop para calcular a potência
+            for( int i = 1; i <= expoente; i++ )
+            {
+                resultado *= valorBase;
+            } // fim for
+
+            return resultado;
+        } // fim método Potencia
+    } // fim classe
+} // fim namespace
