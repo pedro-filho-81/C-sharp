6cf1aab4b913d8614f2a22a9767710b3d9695b61
PedroFilho <psfilho81@hotmail.com>
2021-06-26 18:52:49 -0300

    Inicializando Vetor

---
+++ LivroC#ComoProgramar/Capitulo8/Exemplos/InicializandoVetor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace InicializandoVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            // cria um vetor
+            int[] vetor = new int[5];
+
+            // cria cabeçalho
+            WriteLine($"{"Índice"}{"Valor", 8}");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
