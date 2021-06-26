91e6e8d1a45ed5e9700d6cab27e44201788c7fe3
PedroFilho <psfilho81@hotmail.com>
2021-06-26 16:57:53 -0300

    Atualização

--- LivroC#ComoProgramar/Capitulo7/Exemplos/Recursividade/Fatorial/Program.cs
+++ LivroC#ComoProgramar/Capitulo7/Exemplos/Recursividade/Fatorial/Program.cs
@@ -7,10 +7,29 @@ namespace Fatorial
     {
         static void Main(string[] args)
         {
-            // limpa a tela
+            // limpa a tela 
             Clear();
-            
+
+            // loop para calcular o fatorial
+            for( long contar = 0; contar <= 10; contar++)
+            {
+                WriteLine($"{contar}! = {Fatorial(contar)}" );                
+            } // fim for
+
             Console.WriteLine("Hello World!");
-        }
-    }
-}
+        } // fim main
+
+        // método fatorial
+        static long Fatorial( long numero )
+        {
+            if( numero == 1 || numero == 0 )
+            {
+                return 1;
+            } // fim if
+            else
+            {
+                return numero * Fatorial( numero - 1 );
+            } // fim else
+        } // fim método fatorial
+    } // fim classe
+} // fim namespace
