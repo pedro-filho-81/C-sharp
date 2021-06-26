9b6974c68e84c59fac043c9587bd86e55116756c
PedroFilho <psfilho81@hotmail.com>
2021-06-26 16:47:33 -0300

    Fatorial

---
+++ LivroC#ComoProgramar/Capitulo7/Exemplos/Recursividade/Fatorial/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
