2e3532d2a3d8fa22845ce66368a7b95604550d70
PedroFilho <psfilho81@hotmail.com>
2021-06-28 17:20:49 -0300

    Atualização

--- LivroC#ComoProgramar/Capitulo8/Exemplos/CriandoBarras6/Program.cs
+++ LivroC#ComoProgramar/Capitulo8/Exemplos/CriandoBarras6/Program.cs
@@ -11,24 +11,39 @@ namespace CriandoBarras6
             Clear();
 
             // cria vetor e inicializa
-            int[] vetor = {0, 0, 0, 0, 0, 0, 1, 2, 4, 2, 1 };
+            int[] vetor = {3, 5, 7, 1, 8, 9, 1, 2, 4, 2, 1 };
 
+            // cabeçalho
             WriteLine("Distribuição das notas");
 
-            // para cada elemento do vetor mostre a barra de caractere
-            for( int contar = 0; contar < vetor.Length; contar++ )
+            // loop para cada elemento do vetor mostre a barra de caractere
+            for( int contar = 0; contar < vetor.Length; ++contar )
             {
                 // se contar igual a 10 imprima 100
                 if( contar == 10 )
                 {
+                    // imprima
                     WriteLine(" 100: ");
                 } // fim if
-                else
+                else // se não
                 {
-                    Write($"{contar * 10:D2}-{contat * 10 + 9:D2}: ");
-                }
-            } 
+                    // imprima
+                    Write($"{contar * 10:D2}-{contar * 10 + 9:D2}: ");
+                    
+                    // loop para mostrar as barras de asterisco
+                    for( int estrelas = 0; estrelas < vetor[ contar ]; ++estrelas )
+                    {
+                        Write("*"); // imprima
+
+                    } // fim for asterisco
+
+                    WriteLine(); // pula linha
+
+                } // fim else
+           
+            } // fim for
+            
             Console.WriteLine("Hello World!");
-        }
-    }
-}
+        } // fim main
+    } // fim classe
+} // fim namespace
