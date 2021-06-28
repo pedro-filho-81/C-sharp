9d804dfb44f2aa2f7307fb137cf5488b70fa88fb
PedroFilho <psfilho81@hotmail.com>
2021-06-28 11:45:59 -0300

    Atualização

--- LivroC#ComoProgramar/Capitulo8/Exemplos/SomaValoresDoVetor4/Program.cs
+++ LivroC#ComoProgramar/Capitulo8/Exemplos/SomaValoresDoVetor4/Program.cs
@@ -16,13 +16,29 @@ namespace SomaValoresDoVetor4
             // cria vetor
             int[] vetor = new int[ TAMANHO ];
 
+            // cria o objeto randômico
+            Random numerosAleatorios = new Random();
+
+            // cria variável soma
+            int soma = 0;
+
             // loop para adicionar os valores
             for( int i = 0; i < TAMANHO; i++ )
             {
-                
-            }
+               vetor[ i ] =  numerosAleatorios.Next( 50 );
+            } // fim for adicionar
 
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            WriteLine($"{"Índice"}{"Valor", 8}" );
+
+            // loop para mostrar os valores do vetor
+            for( int i = 0; i < TAMANHO; i++ )
+            {
+                WriteLine($"{ i, 6}{vetor[ i ], 5}");
+
+                soma += vetor[ i ];
+            } // fim for mostrar
+
+            Console.WriteLine($"Soma = {soma}");
+        } // fim main
+    } // fim classe
+} // fim namespace
