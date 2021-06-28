74eff53cd6d48f8d41e32756719b7f023d014301
PedroFilho <psfilho81@hotmail.com>
2021-06-28 11:36:56 -0300

    Soma os Valores do Vetor

---
+++ LivroC#ComoProgramar/Capitulo8/Exemplos/SomaValoresDoVetor4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace SomaValoresDoVetor4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            // cria uma constante
+            const int TAMANHO = 10;
+
+            // cria vetor
+            int[] vetor = new int[ TAMANHO ];
+
+            // loop para adicionar os valores
+            for( int i = 0; i < TAMANHO; i++ )
+            {
+                
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
