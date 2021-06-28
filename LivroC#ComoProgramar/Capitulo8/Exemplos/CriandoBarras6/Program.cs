f6fc56133f6d39056e82d193c4211b6ff15feafd
PedroFilho <psfilho81@hotmail.com>
2021-06-28 16:33:51 -0300

    Barra de Asterisco

---
+++ LivroC#ComoProgramar/Capitulo8/Exemplos/CriandoBarras6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace CriandoBarras6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            // cria vetor e inicializa
+            int[] vetor = {0, 0, 0, 0, 0, 0, 1, 2, 4, 2, 1 };
+
+            WriteLine("Distribuição das notas");
+
+            // para cada elemento do vetor mostre a barra de caractere
+            for( int contar = 0; contar < vetor.Length; contar++ )
+            {
+                // se contar igual a 10 imprima 100
+                if( contar == 10 )
+                {
+                    WriteLine(" 100: ");
+                } // fim if
+                else
+                {
+                    Write($"{contar * 10:D2}-{contat * 10 + 9:D2}: ");
+                }
+            } 
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
