c7ad9e71f2d414e4ceba12eb026bbc574129bfc6
PedroFilho <psfilho81@hotmail.com>
2021-06-28 10:00:31 -0300

    Inicializando Valor

---
+++ LivroC#ComoProgramar/Capitulo8/Exemplos/InicializandoVetor2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace InicializandoVetor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            // cria vetor
+            int[] vetor = {15, 25, 85, 95, 75, 35, 45 };
+
+            WriteLine($"{"Índice"}{"Valor", 8}" );
+
+            // loop para mostrar valores
+            for( int contar = 0; contar < vetor.Length; contar++ )
+            {
+                WriteLine( $"{contar, 5}{vetor[ contar ], 8}" );
+            } // fim for
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
