f68c02b09ff495dd278b9afacff27f17ab25968c
PedroFilho <psfilho81@hotmail.com>
2021-06-28 09:44:57 -0300

    Atualização

--- LivroC#ComoProgramar/Capitulo8/Exemplos/InicializandoVetor/Program.cs
+++ LivroC#ComoProgramar/Capitulo8/Exemplos/InicializandoVetor/Program.cs
@@ -11,11 +11,24 @@ namespace InicializandoVetor
             Clear();
 
             // cria um vetor
-            int[] vetor = new int[5];
+            int[] vetor = new int[10];
 
             // cria cabeçalho
             WriteLine($"{"Índice"}{"Valor", 8}");
+
+            // loop para mostra indice e valor
+            for( int contar = 0; contar < vetor.Length; contar++ )
+            {
+                WriteLine($"{contar, 6}{vetor[ contar ], 6}");
+            } // fim for
+            
             Console.WriteLine("Hello World!");
+        } // fim main
+
+        // método índice
+        static void Indice( int indice )
+        {
+
         }
-    }
-}
+    } // fim classe
+} // fim namespace
