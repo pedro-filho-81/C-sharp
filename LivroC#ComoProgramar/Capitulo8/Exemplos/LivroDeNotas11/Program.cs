18686adc6a3ee7f24cd7ed3c63f2658661df3029
PedroFilho <psfilho81@hotmail.com>
2021-07-01 16:30:43 -0300

    Livro de notas

---
+++ LivroC#ComoProgramar/Capitulo8/Exemplos/LivroDeNotas11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace LivroDeNotas11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
