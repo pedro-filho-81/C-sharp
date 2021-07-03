8acc17f92d5b76a185424341645e2a5e0a9c869d
PedroFilho <psfilho81@hotmail.com>
2021-07-03 12:03:35 -0300

    Criando Formulário Windows

---
+++ LivroC#ComoProgramar/SegundoPrograma/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace SegundoPrograma
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOla = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.textNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOla
+            // 
+            this.btnOla.Location = new System.Drawing.Point(40, 100);
+            this.btnOla.Name = "btnOla";
+            this.btnOla.Size = new System.Drawing.Size(94, 29);
+            this.btnOla.TabIndex = 3;
+            this.btnOla.Text = "&Olá";
+            this.btnOla.UseVisualStyleBackColor = true;
+            this.btnOla.Click += new System.EventHandler(this.btnOla_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(193, 100);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(94, 29);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // textNome
+            // 
+            this.textNome.Location = new System.Drawing.Point(40, 53);
+            this.textNome.Name = "textNome";
+            this.textNome.Size = new System.Drawing.Size(247, 27);
+            this.textNome.TabIndex = 2;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblNome.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblNome.Location = new System.Drawing.Point(40, 21);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(172, 28);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Digite seu nome:";
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(359, 192);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.textNome);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnOla);
+            this.MaximizeBox = false;
+            this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Olá, Mundo C#!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOla;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox textNome;
+        private System.Windows.Forms.Label lblNome;
+    }
+}
+
---
+++ LivroC#ComoProgramar/SegundoPrograma/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SegundoPrograma
+{
+    public partial class FormPrincipal : Form
+    {
+        private string nome;
+
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOla_Click(object sender, EventArgs e)
+        {
+            nome = textNome.Text;
+            MessageBox.Show($"Olá, {nome}! Bem vindo ao C#.", "Olá", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        } // fim btn click
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            textNome.Text = "";
+        } // fim bte limpar
+    } // fim classe
+} // fim namespace
