579c830c57a46a72a687cc8408e34d272dd457aa
PedroFilho <psfilho81@hotmail.com>
2021-07-07 17:31:19 -0300

    Atualização

--- LivroC#ComoProgramar/Capitulo8/Exemplos/Matriz/InicializaMatriz1/Program.cs
+++ LivroC#ComoProgramar/Capitulo8/Exemplos/Matriz/InicializaMatriz1/Program.cs
@@ -11,27 +11,46 @@ namespace InicializaMatriz1
             Clear();
 
             // cria uma matriz retangulas e inicializa
-            int[ , ] matrizRetangular = { {1, 2, 3}, {4, 5, 6} };
+            int[ , ] matrizRetangular = { {1, 2, 3}, { 7, 8, 9 }, {4, 5, 6} };
 
-            int [][] matrizIrregular = { new int[]{1, 2}, new int[]{3}, new int[]{4, 5, 6} };
+            int [][] matrizIrregular = { new int[]{1, 2}, 
+                                        new int[]{3},
+                                        new int[]{9, 8, 7, 6, 5 }, 
+                                        new int[]{4, 5, 6} };
+
+            // chama métodos
+            MostrarMatriz(matrizRetangular);
+            MostrarElementos(matrizIrregular);
+            WriteLine();
+
+        } // fim main
+
+        // criar método mostrar matriz
+        static void MostrarMatriz( int[,] matriz )
+        {
+            WriteLine("Mostra os valores da matriz retangular." );
 
             // loop para crair linha
-            for( int linha = 0; linha < matrizRetangular.GetLength(0); linha++ )
+            for( int linha = 0; linha < matriz.GetLength(0); linha++ )
             {
                 // loop para criar coluna
-                for(int coluna = 0; coluna < matrizRetangular.GetLength(1); coluna++ )
+                for(int coluna = 0; coluna < matriz.GetLength(1); coluna++ )
                 {
                     // mostra os elementos da matriz
-                   Write( $"{matrizRetangular[linha, coluna ]} " );
+                   Write( $"{matriz[linha, coluna ]} " );
                 } // fim for coluna
 
                 WriteLine(); // pula linha
 
             } // fim for
+        } // fim método
 
-            WriteLine();
-
-            foreach( var linha in matrizIrregular )
+        // cria método mostrar elementos
+        static void MostrarElementos( int[][] matriz)
+        {
+            WriteLine("Mostrar elementos da matriz.");
+            
+            foreach( var linha in matriz )
             {
                 foreach( var elemento in linha )
                 {
@@ -41,6 +60,6 @@ namespace InicializaMatriz1
                 WriteLine(); // pula linha
             } // fim foreach
 
-        } // fim main
+        } // fim método
     } // fim clase
 } // fim name
