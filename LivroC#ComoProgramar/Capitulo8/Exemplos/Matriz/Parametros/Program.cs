50cd4c3c4d5ebdbae2650bcc4cf7b221b958440c
PedroFilho <psfilho81@hotmail.com>
2021-07-08 16:52:56 -0300

    Update Program.cs

--- LivroC#ComoProgramar/Capitulo8/Exemplos/Matriz/Parametros/Program.cs
+++ LivroC#ComoProgramar/Capitulo8/Exemplos/Matriz/Parametros/Program.cs
@@ -31,7 +31,6 @@ namespace Parametros
             // mostra as variáveis e seus valores
             WriteLine($"d1 = {d1:f1}\nd2 = {d2:f1}\nd3 = {d3:f1}\nd4 = {d4:f1}");
 
-
             // mostra a média entre os valores
             WriteLine($"A média entre d1 e d2 é {Media(d1, d2):f2}");
 
