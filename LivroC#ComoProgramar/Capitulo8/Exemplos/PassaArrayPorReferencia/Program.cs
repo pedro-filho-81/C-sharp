af31ba3f4a99b59f2152fb3e9404307712514151
PedroFilho <psfilho81@hotmail.com>
2021-07-11 09:22:48 -0300

    Passando Array por Referência

---
+++ LivroC#ComoProgramar/Capitulo8/Exemplos/PassaArrayPorReferencia/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace PassaArrayPorReferencia
+{
+    class Program
+    {
+        /*
+            Testando a passagem de array por referencia, valor valor e referência
+        */
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            // criando e inicializando o primeiro array
+            int [] array = {1, 2, 3 };
+
+            //
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
