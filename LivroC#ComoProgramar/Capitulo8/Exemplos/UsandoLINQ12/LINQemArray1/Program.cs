b014737dc7f7a52ac38d74cfb4c333e6e141a75e
PedroFilho <psfilho81@hotmail.com>
2021-07-12 10:12:13 -0300

    Trabalhando com LINQ

---
+++ LivroC#ComoProgramar/Capitulo8/Exemplos/UsandoLINQ12/LINQemArray1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using static System.Console;
+using System.Linq;
+
+namespace LINQemarray1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            // cria um array
+            int[] valores = {2, 0, 9, 8, 1, 3, 4, 5, 7, 6};
+
+            // mostra os valores originais
+            WriteLine("Mostra os valores originais do array: ");
+
+            // loop para mostra os valores do vetor
+            foreach( var valor in valores)
+            {
+                Write($"{valor} " );
+            } // fim foreach
+
+            // pula linha
+            WriteLine();
+
+            // LINQ para obter os valores do array maiores que 4
+            var filtrar =
+                from valor in valores
+                where valor > 4
+                select valor;
+
+            WriteLine("Mostra os valores do array maiores que 4:" );
+
+            // mostra o resultado da filtragem
+            foreach( var valor in filtrar )
+            {
+                Write($"{valor} ");
+            } // fim foreach
+            
+            // LINQ para ordenar os valores do array
+            WriteLine("\nMortrar os valores do array em ordem crescente.");
+
+            var ordenar =
+                from valor in valores
+                orderby valor
+                select valor;
+
+            // loop para mostrar os valores ordenado
+            foreach( var valor in ordenar )
+            {
+                Write($"{valor} " );
+            } // fim foreach
+
+            WriteLine("\nMostra os valores do array em ordem decrescente:");
+
+            // LINQ para mostrar os valores ordenado em ordem decrescente
+            var ordemDecrescente = 
+                from valor in valores
+                orderby valor descending
+                select valor;
+
+            // loop para mostra os valores
+            foreach( var valor in ordemDecrescente )
+            {
+                Write($"{valor} ");
+            }// fim foreach
+
+            // LINQ para mostrar os valores maiores que 4 em ordem decrescente
+
+            Console.WriteLine("\nHello World!");
+        } // fim main
+    } // fim classe
+} // fim name
