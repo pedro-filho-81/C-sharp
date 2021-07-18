aac270619b6ed8473102c0b5c8bbd8bec57c11f1
PedroFilho <psfilho81@hotmail.com>
2021-07-18 19:17:38 -0300

    Update Program.cs

--- LivroC#ComoProgramar/Capitulo3/Exemplos/OlaMundo/Program.cs
+++ LivroC#ComoProgramar/Capitulo3/Exemplos/OlaMundo/Program.cs
@@ -7,6 +7,9 @@ namespace OlaMundo
         // método main é o início da execução de um aplicativo C#
         static void Main(string[] args)
         {
+            // limpa a tela
+            Console.Clear();
+            
             Console.WriteLine("Bem vindo a programação C#!");
         } // fim main
     } // fim classe
