736e2098e5c4708b51bd27edd542fca592f41fd1
PedroFilho <psfilho81@hotmail.com>
2021-07-18 19:15:56 -0300

    Olá Mundo

---
+++ LivroC#ComoProgramar/Capitulo3/Exemplos/OlaMundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        // método main é o início da execução de um aplicativo C#
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo a programação C#!");
+        } // fim main
+    } // fim classe
+} // fim name
