bda498f17e5ce9dbc6fafc21322ecc8ecbd801f5
PedroFilho <psfilho81@hotmail.com>
2021-07-19 10:16:56 -0300

    Instrução If

---
+++ LivroC#ComoProgramar/Capitulo3/Exemplos/InstrucaoIf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InstrucaoIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
