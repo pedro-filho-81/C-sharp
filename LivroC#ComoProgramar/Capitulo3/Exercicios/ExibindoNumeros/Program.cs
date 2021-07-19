888c53b9027fbe95b5ba9317ea3b436c1e5ef859
PedroFilho <psfilho81@hotmail.com>
2021-07-19 11:18:09 -0300

    Update Program.cs

--- LivroC#ComoProgramar/Capitulo3/Exercicios/ExibindoNumeros/Program.cs
+++ LivroC#ComoProgramar/Capitulo3/Exercicios/ExibindoNumeros/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace ExibindoNumeros
 {
@@ -6,7 +7,19 @@ namespace ExibindoNumeros
     {
         static void Main(string[] args)
         {
+            // limpa a tela
+            Clear();
+            // mostra os números de 1 a 4 em uma linha
+            WriteLine("1 2 3 4");
+
+            // mostra os números de 1 a 4 em uma linha
+            Write("1 ");
+            Write("2 ");
+            Write("3 ");
+            Write("4\n"); // \n pula uma linha
+
+            // mostra 
             Console.WriteLine("Hello World!");
-        }
-    }
-}
+        } // fim main
+    } // fim classe
+} // fim name
