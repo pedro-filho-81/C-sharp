fc5c76350c91cc2f4e99a1e5d611466add7d601d
PedroFilho <psfilho81@hotmail.com>
2021-07-19 10:02:07 -0300

    Variáveis Recebem Valores

---
+++ LivroC#ComoProgramar/Capitulo3/Exemplos/VariaveisRecebemValores/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace VariaveisRecebemValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            // cria variáveis
+            int numero1;
+            int numero2;
+            int soma;
+
+            // entrada de dados
+            Console.Write("Digite o primeiro valor: ");
+            numero1 = int.Parse(ReadLine());
+
+            Write("Digite o segundo valor: " );
+            numero2 = int.Parse(ReadLine());
+
+            // calcular a soma
+            soma = numero1 + numero2;
+
+            // mostra a soma
+            WriteLine($"A soma entre {numero1} e {numero2} é {soma}\n");
+
+        } // fim main
+    } // fim classe
+} // fim name
