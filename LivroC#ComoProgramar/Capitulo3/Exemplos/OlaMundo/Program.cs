1d0e41ae38057afd0acb69d158dfbf325153c1ce
PedroFilho <psfilho81@hotmail.com>
2021-07-19 09:14:06 -0300

    Update Program.cs

--- LivroC#ComoProgramar/Capitulo3/Exemplos/OlaMundo/Program.cs
+++ LivroC#ComoProgramar/Capitulo3/Exemplos/OlaMundo/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
-namespace OlaMundo
+namespace OlaMundo // 
 {
-    class Program
+    class Program // declara a classe Program
     {
         // método main é o início da execução de um aplicativo C#
         static void Main(string[] args)
@@ -10,7 +10,15 @@ namespace OlaMundo
             // limpa a tela
             Console.Clear();
             
+            // mostra olá mundo na tela
             Console.WriteLine("Bem vindo a programação C#!");
+
+            // mostra bem vindo, uma palavra em baixo da outra
+            Console.WriteLine("\nBem\nvindo\na\nprogramação\nC#.");
+
+            // mostra uma sequência de escape com \n
+            Console.WriteLine("\nO caractere de escape \\ combinado com n significa nova linha\\n\n");
+            
         } // fim main
     } // fim classe
 } // fim name
