5dbc7b7084ee9ad39f0d0aaac45ad7a661ddd696
PedroFilho <psfilho81@hotmail.com>
2021-07-19 09:36:33 -0300

    Interpolação de String

---
+++ LivroC#ComoProgramar/Capitulo3/Exemplos/InterpolacaoDeString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace InterpolacaoDeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            // variável
+            string nome = "Pedro";
+
+            // interpolação de string
+            Console.WriteLine($"Bem vindo a programação C#, {nome}!");
+            // o símbolo $ permite a interpolação
+
+        } // fim main
+    } // fim classe
+} // fim name
