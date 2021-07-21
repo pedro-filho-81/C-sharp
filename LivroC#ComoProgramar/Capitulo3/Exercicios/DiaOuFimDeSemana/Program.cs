2b8b913d321769e4c318a6c9ee5542700560dd5d
PedroFilho <psfilho81@hotmail.com>
2021-07-21 18:21:24 -0300

    Dia ou Fim de Semana

---
+++ LivroC#ComoProgramar/Capitulo3/Exercicios/DiaOuFimDeSemana/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace DiaOuFimDeSemana
+{
+    class Program
+    {
+        /*
+            3.16 (Dia da semana / fim de semana) Escreva um aplicativo 
+            que peça ao usuário para inserir um número inteiro e exibe as 
+            palavras "dia útil" se a entrada for de 1 a 5 ou "fim de semana" 
+            se a entrada for 6 ou 7. Qualquer outra entrada deve ser exibida 
+            "inválido". Use as técnicas mostradas na Fig. 3.22.
+            Autor: Pedro Filho, 21/07/2021
+        */
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
