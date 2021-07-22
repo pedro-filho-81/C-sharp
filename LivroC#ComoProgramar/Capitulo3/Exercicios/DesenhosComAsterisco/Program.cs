ac3778487304b04f3d9ed40035ce8b20e1f5049e
PedroFilho <psfilho81@hotmail.com>
2021-07-22 09:39:56 -0300

    Desenhos com Asteriscos

---
+++ LivroC#ComoProgramar/Capitulo3/Exercicios/DesenhosComAsterisco/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace DesenhosComAsterisco
+{
+    class Program
+    {
+        /*
+            3.18 (Exibindo formas com asteriscos) Escreva um aplicativo que exiba uma caixa, 
+            uma oval, uma seta e um diamante usando asteriscos (*), como na Fig. 3.25.
+            Autor: Pedro Filho, 22/07/2021
+        */
+
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            Console.WriteLine("DESENHOS COM ASTERISCOS");
+
+            // mostrando desenhos
+            WriteLine("**********       ***         *          *");
+            WriteLine("*        *     *     *      ***        * *");
+            WriteLine("*        *    *       *    *****      *   *");
+            WriteLine("*        *    *       *      *       *     *");
+            WriteLine("*        *    *       *      *      *       *");
+            WriteLine("*        *    *       *      *       *     *");
+            WriteLine("*        *    *       *      *        *   *");
+            WriteLine("*        *     *     *       *         * *");
+            WriteLine("**********       ***         *          *");
+        } // fim main
+    } // fim classe
+} // fim name
