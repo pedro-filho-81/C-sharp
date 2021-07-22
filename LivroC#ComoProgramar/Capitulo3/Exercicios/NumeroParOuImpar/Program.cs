7063758704c14466c722f64f1a8582551f92f5a7
PedroFilho <psfilho81@hotmail.com>
2021-07-22 09:50:38 -0300

    Número Par ou Ímpar

---
+++ LivroC#ComoProgramar/Capitulo3/Exercicios/NumeroParOuImpar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NumeroParOuImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Console.Clear();
+
+            // entrada de dados
+            Console.Write("Digite um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            // se o número multiplo de 2
+            if( numero % 2 == 0 )
+            {
+                Console.WriteLine($"{numero} É PAR.");
+            } // FIM IF
+
+            if( numero %2 != 0 )
+            {
+                Console.WriteLine($"{numero} É ÍMPAR.");
+            } // fim if
+            
+        } // fim main
+    } // fim classe
+} // fim name
