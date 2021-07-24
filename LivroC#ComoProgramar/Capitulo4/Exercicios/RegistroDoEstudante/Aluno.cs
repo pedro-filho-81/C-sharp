9ef030a5bf09d30d7b69371cc3980f146658aa85
PedroFilho <psfilho81@hotmail.com>
2021-07-24 12:02:00 -0300

    Atualização

---
+++ LivroC#ComoProgramar/Capitulo4/Exercicios/RegistroDoEstudante/Aluno.cs
@@ -0,0 +1,69 @@
+using System;
+using static System.Console;
+
+class Aluno
+{
+    // cria as variáveis
+    private decimal nota1;
+    private decimal nota2;
+    private decimal nota3;
+
+    // cria propriedades
+    public string IdDoAluno { get; set; }
+    public string Nome { get; set; }
+    public decimal Nota1 { 
+                            get
+                            {
+                                return nota1 ;
+                            } // fim get
+                            set
+                            {
+                                if( value > 0.0m )
+                                {
+                                    nota1 = value;
+                                } // fim if
+                            } // fim set
+                            
+                        } // fim propriedade
+
+    public decimal Nota2 { 
+                            get
+                            {
+                                return nota2 ;
+                            } // fim get
+                            set
+                            {
+                                if( value > 0.0m )
+                                {
+                                    nota2 = value;
+                                } // fim if
+                            } // fim set
+                            
+                        } // fim propriedade
+
+    public decimal Nota3 { 
+                            get
+                            {
+                                return nota3;
+                            } // fim get
+                            set
+                            {
+                                if( value > 0.0m )
+                                {
+                                    nota3 = value;
+                                } // fim if
+                            } // fim set
+                            
+                        } // fim propriedade
+
+    // cria o construtor
+    public Aluno( string identidade, string nome, decimal nota1, decimal nota2, decimal nota3 )
+    {
+        IdDoAluno = identidade;
+        Nome = nome;
+        Nota1 = nota1;
+        Nota2 = nota2;
+        Nota3 = nota3;
+    } // fim construtor
+
+} // fim classe
