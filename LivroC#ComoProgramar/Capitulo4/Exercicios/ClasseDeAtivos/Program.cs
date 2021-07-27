f41f93baf6f2824f83b70a9ee6e3477a52e411d5
PedroFilho <psfilho81@hotmail.com>
2021-07-27 10:56:13 -0300

    Classe de Ativos

---
+++ LivroC#ComoProgramar/Capitulo4/Exercicios/ClasseDeAtivos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace ClasseDeAtivos
+{
+    class Program
+    {
+        /*
+            4.11 (Classe de ativo) Crie uma classe chamada Ativo que inclui três informações 
+            como variáveis de instância ou propriedades autoimplementadas - nome do ativo 
+            (tipo string), valor do ativo (tipo decimal) e taxa de depreciação (tipo decimal). 
+            Sua classe deve ter um construtor que inicializa os três valores. 
+            Fornece uma propriedade com um acessador get e set para quaisquer variáveis de instância. 
+            Se o valor do ativo ou a taxa de depreciação for negativa, o acessador set deve deixar 
+            a variável de instância inalterada. Escreva um aplicativo de teste denominado AssetTest 
+            que demonstra os recursos da classe Asset. Crie dois objetos Ativos e exiba a quantidade 
+            de depreciação de cada objeto e seu valor depreciado. Em seguida, aumente o valor de cada 
+            ativo em 5% e exiba o valor de depreciação de cada ativo e seu valor depreciado.
+        */
+        static void Main(string[] args)
+        {
+            // limpa a tela
+            Clear();
+
+            Console.WriteLine("Hello World!");
+        } // fim main
+    } // fim classe
+} // fim name
