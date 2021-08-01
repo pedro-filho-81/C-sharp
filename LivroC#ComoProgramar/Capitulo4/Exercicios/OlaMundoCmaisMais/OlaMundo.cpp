518076b4768ced226dbfcf6547da3f775c9a2179
PedroFilho <psfilho81@hotmail.com>
2021-08-01 09:54:51 -0300

    Olá Mundo C++

---
+++ LivroC#ComoProgramar/Capitulo4/Exercicios/OlaMundoCmaisMais/OlaMundo.cpp
@@ -0,0 +1,17 @@
+// permite trabalhar com objetos de entrada e saída de dados (cout, cin)
+#include <iostream> // biblioteca de arquivos de cabeçalho
+
+using namespace std; // biblioteca padrão onde está localizado( cout / cin)
+
+// função principal para execução do programa c++
+int main()
+{ // limite inicial para a execução do programa (É Obrigatório)
+
+    // escrave na tela o que está entre aspas
+    // cout objeto 
+    cout << "Olá, Mundo C++!";
+
+    // indica que o programa terminou sem problemas
+    return 0;
+
+} // fim main (fim do limite obrigatório para o programa C++ )
