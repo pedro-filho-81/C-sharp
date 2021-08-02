cae8c4ba503d8b8af62dd19f1adab794ece48e02
PedroFilho <psfilho81@hotmail.com>
2021-08-02 18:48:48 -0300

    Atualização

--- LivroC#ComoProgramar/Capitulo5/Exercicios/LimiteDeCredito/Program.cs
+++ LivroC#ComoProgramar/Capitulo5/Exercicios/LimiteDeCredito/Program.cs
@@ -5,38 +5,54 @@ namespace LimiteDeCredito
 {
     class Program
     {
+        /*
+            5.17 (Juros sobre Depósito a Prazo) Os investidores estão preocupados com os juros 
+            que seus investimentos ganham em um mês. Um investidor acompanhou os juros ganhos 
+            em vários investimentos, registrando o valor de cada investimento e os juros ganhos 
+            em cada um. Desenvolva um aplicativo C # que irá inserir o valor investido e os juros 
+            ganhos (ambos como números inteiros) para cada investimento. O aplicativo deve calcular 
+            e exibir os juros combinados ganhos para cada investimento até o mês atual. 
+            Os resultados são arredondados para o centésimo mais próximo. Use o método ReadLine da 
+            classe Console e a iteração controlada por sentinela para obter os dados do usuário.
+            Autor: Pedro Filho, 02/08/2021
+        */
         static void Main(string[] args)
         {
             // limpa a tela
             Clear();
 
             // variáveis
-            decimal resultadoJuros;
+            int valorInicial;
+            int taxaDeJuros;
+            int tempoInvestido;
+            int jurosGanhos;
 
-            // cria o objeto da classe Credito
-            Credito investimento = new Credito( 100, 500, 5, 4 );
+            // entrada de dados
+            Write("Digite o valor inicial ( -1 para sair ) R$" );
+            valorInicial = int.Parse(ReadLine());
 
-            resultadoJuros = investimento.Juros( 500, 5, 4 );
+            // enquanto o valor inicial diferente de -1
+            while ( valorInicial != -1 )
+            {
+                Write("Digite a taxa de juros: ");
+                taxaDeJuros = int.Parse(ReadLine());
 
-            // chama o método MostraResultado
-            MostraResultado( investimento, resultadoJuros );
+                Write("Informe o tempo da aplicação: " );
+                tempoInvestido = int.Parse(ReadLine());
+
+                decimal juros = (decimal) valorInicial * taxaDeJuros * tempoInvestido / 365;
+
+                WriteLine($"Valor inicial R${valorInicial}" +
+                            $"\nTaxa de juros {taxaDeJuros}%" +
+                            $"\nValor dos juros R${juros:C}");
+
+                // entrada de dados
+                Write("\nDigite o valor inicial ( -1 para sair ) R$" );
+                valorInicial = int.Parse(ReadLine());
+
+            } // fim while
 
             Console.WriteLine("Hello World!");
         } // fim main
-
-        // cria método MostraResultado
-        static void MostraResultado(Credito meuInvestimento, decimal jurosGanhos )
-        {
-            // cabeçalho
-            WriteLine("INVESTIMENTO\t\tDADOS" );
-
-            // MOSTRA resultado
-            WriteLine($"Número da conta:\t{meuInvestimento.numeroDaConta}" +
-                        $"\nValor investido:\t{meuInvestimento.ValorInvestido}" +
-                        $"\nTaxa de juros:\t\t{meuInvestimento.TaxaDeJuros}%" +
-                        $"\nTempo investido:\t{meuInvestimento.TempoAplicado} mêses" +
-                        $"\nJuros ganhos:\t\t{jurosGanhos:C}");
-        } // fim método
-
     } // fim classe
 } // fim name
